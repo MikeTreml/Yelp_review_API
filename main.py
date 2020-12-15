f090c7aba43da9ef3d5f6b2822086d01cc9db3a8
michaeltreml <miketreml@mac.com>
2020-12-15 13:06:53 -0600

    sending list_reviews to the good face detections. Printing the json and also saving it as a file.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ yelp_id_search ='swingin-door-exchange-milwaukee'
 
 def main():
     list_reiews = yelp_review_api.get_business_reviews(yelp_id_search)
+    list_reiews_joy = google_vision.add_joy_likelihood(list_reiews)
+    final_json = json.dumps(list_reiews_joy)
+    print(final_json)
+    with open("final_json.json", "w") as file:
+        json.dump(final_json, file)
 
 
 if __name__ == '__main__':
