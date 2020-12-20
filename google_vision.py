0a43bfd96644ea4babeb98952b3660109615d782
michaeltreml <miketreml@mac.com>
2020-12-20 11:16:21 -0600

    added likelihood_name as numbers for better readability.

--- google_vision.py
+++ google_vision.py
@@ -2,17 +2,6 @@ import os
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/michaeltreml/Documents/g_vision.json"
 
-def add_joy_likelihood(list_reiews):
-    result_list = []
-    for review in list_reiews:
-        #send the user image to the face detetion grab a list of joy_lilelihoods. Number returned is based on each face detected
-        joy_list = detect_faces_uri(review['user']['image_url'])
-        # add a new variable of joy_list to the user Dictionary
-        review['user']['likelihood'] = joy_list
-        result_list.append(review)
-
-    return result_list
-
 
 def detect_faces_uri(uri):
     """Detects faces in the file located in Google Cloud Storage or the web."""
@@ -23,10 +12,10 @@ def detect_faces_uri(uri):
     response = client.face_detection(image=image)
     faces = response.face_annotations
     #safe = response.safe_search_annotation
-    list_joy_likelihood = []
 
-    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
-                       'LIKELY', 'VERY_LIKELY')
+    list_joy_likelihood = []
+    #likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE', 'LIKELY', 'VERY_LIKELY')
+    likelihood_name = ('UNKNOWN', '0', '1', '2', '3', '4')
     #add joylikelihood to a list to be returned.
     for face in faces:
         dict = {}
