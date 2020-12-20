bd7e9fe1fd740e884a602460cd8e15f49f63acff
michaeltreml <miketreml@mac.com>
2020-12-20 11:18:41 -0600

    moved the list parse function to the correct file. and added some comments to help walkthrough

--- yelp_review_api.py
+++ yelp_review_api.py
@@ -1,10 +1,23 @@
 import requests
 import api_keys
-
+import google_vision
 
 API_HOST = 'https://api.yelp.com'
 BUSINESS_PATH = '/v3/businesses/'
 REVIEWS_PATH = '/reviews'
+
+# this function parses through a list of reviews to rune the joylikelihood
+def add_joy_likelihood(list_reiews):
+    result_list = []
+    for review in list_reiews:
+        #send the user image to the face detetion grab a list of joy_lilelihoods. Number returned is based on each face detected
+        joy_list = google_vision.detect_faces_uri(review['user']['image_url'])
+        # add a new variable of joy_list to the user Dictionary
+        review['user']['likelihood'] = joy_list
+        result_list.append(review)
+
+    return result_list
+
 
 # build the URL
 def get_business_reviews(business_id):
@@ -17,10 +30,14 @@ def api_request(host, path):
     headers = {
         'Authorization': 'Bearer %s' % api_keys.API_KEY,
     }
+    # requests the json review info
     response = requests.request('GET', url, headers=headers)
-    # grab the json review info
+
+    # saves the json reviews info
     reviews_data = response.json()
+
     # save the json as a list of Dictionaries
     list_reviews = reviews_data['reviews']
+
     return list_reviews
 
